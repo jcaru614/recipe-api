dad26d4b23216ec59a3c8b69c85b685f1091a4e6
Joseph Caruana <jcaruana614@gmail.com>
2020-04-09 18:20:20 -0700

    travis fixed

--- app/core/models.py
+++ app/core/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new user"""
+        """creates and saves a new user"""
         if not email:
             raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
@@ -16,7 +16,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        """Creates and saves a superuser user"""
+        """creates and saves a superuser user"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
