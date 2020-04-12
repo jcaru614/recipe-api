2d0f20be82d5a8606618ed509afe6cac7d628db5
Joseph Caruana <jcaruana614@gmail.com>
2020-04-11 21:03:55 -0700

    added image to model

--- app/core/models.py
+++ app/core/models.py
@@ -1,14 +1,23 @@
+import uuid
+import os
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
-
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/recipe/', filename)
 
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """creates and saves a new user"""
+        """Creates and saves a new user"""
         if not email:
             raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
@@ -18,7 +27,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        """creates and saves a superuser user"""
+        """Creates and saves a superuser user"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -40,7 +49,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 
 class Tag(models.Model):
-    """tag to be used in a recepi"""
+    """Tag to be used for a recipe"""
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -75,6 +84,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,11 +1,13 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 from core import models
 
 
-def sample_user(email='test@gmail.com', password='testpass'):
-    """create sample user"""
+def sample_user(email='test@email.com', password='testpass'):
+    """Create a sample user"""
     return get_user_model().objects.create_user(email, password)
 
 
@@ -46,10 +48,10 @@ class ModelTests(TestCase):
         self.assertTrue(user.is_staff)
 
     def test_tag_str(self):
-        """test the tag string representation"""
+        """Test the tag string representation"""
         tag = models.Tag.objects.create(
             user=sample_user(),
-            name='chicken dish',
+            name='Vegan'
         )
 
         self.assertEqual(str(tag), tag.name)
@@ -67,9 +69,19 @@ class ModelTests(TestCase):
         """Test the recipe string representation"""
         recipe = models.Recipe.objects.create(
             user=sample_user(),
-            title='Carbonara',
+            title='Steak and mushroom sauce',
             time_minutes=5,
             price=5.00
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test that image is saved in the correct location"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
